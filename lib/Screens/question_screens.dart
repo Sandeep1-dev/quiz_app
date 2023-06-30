4f5ce29435f89914bc2dd2d8ad088a30a85323e4
Sandeep1-dev <Sanbook1.0.0.0@gmail.com>
2023-06-30 17:50:07 +0530

    Question Screen

--- lib/Screens/question_screens.dart
+++ lib/Screens/question_screens.dart
@@ -10,6 +10,35 @@ class Questions extends StatefulWidget {
 class _QuestionsState extends State<Questions> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "The Questions...",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Answer-1"),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Answer-2"),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Answer-3"),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("Answer-4"),
+          )
+        ],
+      ),
+    );
   }
 }
